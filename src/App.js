922958a9a004a42862f36063b3f9c781b8049d17
tejakoheda <kohedaraviteja@gmail.com>
2025-09-30 15:26:12 +0530

    first component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-function App(){
-  let names="raviteja"
-  let a=10;
-  let age=21;
-  return <h1>hello,{names}! your age is {a+age}</h1>
+// function App(){
+//   let names="raviteja"
+//   let a=10;
+//   let age=21;
+//   return <h1>hello,{names}! your age is {a+age}</h1>
+// }
+// // export default App;
+
+import Hello from "./Hello";
+function App() {
+  return (
+    <div>
+      <h1>My App</h1>
+      <Hello />
+    </div>
+  );
 }
+
 export default App;

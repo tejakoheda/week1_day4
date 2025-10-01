17ea7073850e7060796d7b317f0fc048bf371ff5
tejakoheda <kohedaraviteja@gmail.com>
2025-10-01 14:37:25 +0530

    if statement ternary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-function App(){
-  let a=10;
-  let b="apple";
-  if(a>5){
-    return <h1>{b}</h1>
-  }else{
-    return <h1>fruit</h1>
-  }
+function fruit() {
+  const x = 5;
+  return (
+    <h1>{x < 10 ? "Banana" : "Apple"}</h1>
+  );
 }
-export default App;
+export default fruit;

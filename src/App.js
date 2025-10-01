2abc82f2580707ddc9473982bcc69ee06377429b
tejakoheda <kohedaraviteja@gmail.com>
2025-10-01 13:16:03 +0530

    if statement

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import Welcome, { Study } from "./Hello";  
-
-function App() {
-  return (
-    <div>
-      <Welcome />
-      <Study />
-    </div>
-  );
+function App(){
+  let a=10;
+  let b="apple";
+  if(a>5){
+    return <h1>{b}</h1>
+  }else{
+    return <h1>fruit</h1>
+  }
 }
-
 export default App;

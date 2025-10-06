212cfd65454fd699117202b31fce389d0ddfc895
tejakoheda <kohedaraviteja@gmail.com>
2025-10-06 12:45:15 +0530

    usestate

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import AlertBox from "./prop/Firstcomp";
+import { useState } from "react";
 
-
-
-function App() {
-  // Function defined in parent
-  const showMessage = () => {
-    alert("Hello Teja! You clicked the button.");
-  };
+function Counter() {
+  const [count, setCount] = useState(0); 
 
   return (
     <div>
-      {/* Sending function as prop (onAlert) */}
-      {/* Sending content as children */}
-      <AlertBox onAlert={showMessage}>
-        <h3>Click the button to see an alert message!</h3>
-      </AlertBox>
+      <h2>Count: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(0)}>Reset</button>
     </div>
   );
 }
 
-export default App;
+export default Counter;

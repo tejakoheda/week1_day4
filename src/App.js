d7b047efb54f6b493839d779224b79452c5945c7
tejakoheda <kohedaraviteja@gmail.com>
2025-10-06 12:55:45 +0530

    usestate2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import { useState } from "react";
 
-function Counter() {
-  const [count, setCount] = useState(0); 
+function ToggleText() {
+  const [show, setShow] = useState(true);
 
   return (
     <div>
-      <h2>Count: {count}</h2>
-      <button onClick={() => setCount(count + 1)}>Increase</button>
-      <button onClick={() => setCount(count - 1)}>Decrease</button>
-      <button onClick={() => setCount(0)}>Reset</button>
+      {show ? <p>Hello</p> : <p>Text Hidden</p>}
+      <button onClick={() => setShow(!show)}>
+        {show ? "Hide Text" : "Show Text"}
+      </button>
     </div>
   );
 }
 
-export default Counter;
+export default ToggleText;
+
